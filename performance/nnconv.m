9c9c57d5a63b76acdfe703c840ff8720346cd45e
MooGuZ <moogu@live.cn>
2017-11-14 19:44:59 -0500

    add support to versions before 2017a and batchsize

--- performance/nnconv.m
+++ performance/nnconv.m
@@ -1,5 +1,9 @@
-function nnconv(probScale, ntry)
-useGPU = logical(gpuDeviceCount);
+function [bir, mir] = nnconv(probScale, ntry, batchsize)
+useGPU  = logical(gpuDeviceCount);
+verstr  = version('-release');
+ver2017 = str2double(verstr(1:4)) >= 2017;
+% default values
+if not(exist('batchsize', 'var')), batchsize = 1; end
 % calculate fundamental paramters
 frmsize  = [probScale, probScale];
 nchannel = ceil(log2(probScale));
@@ -26,23 +30,29 @@ datagen = DataGenerator('normal', [frmsize, nchannel]);
 bitime = 0; % timer of build-in function 
 mitime = 0; % timer of my implementation
 for i = 1 : ntry
-    data = datagen.next().data;
+    data = datagen.next(batchsize).data;
     if useGPU
         data = gpuArray(data);
     end
     % run my implementation
     tstart = tic;
-    MathLib.nnconv(data, f, b, 'same');
+    mir = MathLib.nnconv(data, f, b, 'same');
     etime = toc(tstart);
     mitime = mitime + etime;
     % run build-in function
-    tstart = tic; 
-    bifunc(data, f, padding{:}, 1, 1) + b; 
-    etime = toc(tstart);
+    if ver2017
+        tstart = tic;
+        bir = bifunc(data, f, padding{:}, 1, 1) + b;
+        etime = toc(tstart);
+    else
+        tstart = tic; 
+        bir = bifunc(data, f, padding{1}, padding{2}, 1, 1) + b; 
+        etime = toc(tstart);
+    end
     bitime = bitime + etime;
 end
+bitime = bitime / batchsize / ntry;
+mitime = mitime / batchsize / ntry;
 % print results
-fprintf('Average Running Time for Build-In Function : %.4e\n', bitime / ntry);
-fprintf('Average Running Time for My Implementation : %.4e\n', mitime / ntry);
-
-        
+fprintf('Average Running Time for Build-In Function : %.4e\n', bitime);
+fprintf('Average Running Time for My Implementation : %.4e\n', mitime);
